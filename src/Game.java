36864d2bfeb8df91b4419049e557698430b7c131
Thomas <t.wild155@gmail.com>
2022-10-25 07:16:23 +0300

    WIP - drawing 'images to screen

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,17 @@
 import javax.swing.JFrame;
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import graphics.Screen;
 
 /**
  * Game
  */
-public class Game extends Canvas implements Runnable{
+public class Game extends Canvas implements Runnable {
 
     public static int width = 300;
     public static int height = width / 16 * 9;
@@ -16,50 +21,73 @@ public class Game extends Canvas implements Runnable{
     private JFrame frame;
     private boolean running = false;
 
-    public Game(){
+    private Screen screen;
+    private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
+    public Game() {
         Dimension size = new Dimension(width * scale, height * scale);
         setPreferredSize(size);
 
         frame = new JFrame();
+        screen = new Screen(width, height);
     }
 
-    public synchronized void start(){
+    public synchronized void start() {
         running = true;
         thread = new Thread(this, "Display");
         thread.start();
     }
 
-    public synchronized void stop(){
+    public synchronized void stop() {
         running = false;
-        try{
+        try {
             thread.join();
-        }
-        catch(InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    public void run(){
-        while(running){
+    public void run() {
+        while (running) {
             update();
             render();
         }
     }
 
-    public void update(){
+    public void update() {
 
     }
 
-    public void render(){
+    public void render() {
         BufferStrategy bs = getBufferStrategy();
-        if(bs == null) createBufferStrategy(3);
-        
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+
+        screen.render();
+        for (int i = 0; i < pixels.length; i++) {
+            pixels[i] = screen.pixels[i];
+        }
+
+        Graphics g = bs.getDrawGraphics();
+
+        // fill screen with black
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+        // draw image
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+
+        g.dispose();
+        bs.show();
     }
 
     public static void main(String[] args) {
         Game game = new Game();
         game.frame.setResizable(false);
-        game.frame.setTitle("Wow");
+        game.frame.setTitle("Game");
         game.frame.add(game);
         game.frame.pack();
         game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
